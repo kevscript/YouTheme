a0e40336b48ed78722896ab858a22026e798a5b0
Kevin Ostafinski <kevinostafinski@gmail.com>
2019-11-22 14:45:33 +0100

    created graphql folder on client and moved all Mutations inside to debloat App component

---
+++ client/src/graphql/mutations.js
@@ -0,0 +1,92 @@
+import gql from 'graphql-tag'
+
+export const CREATE_THEME = gql`
+  mutation CreateTheme($id: String!, $themeName: String!) {
+    createTheme(id: $id, themeName: $themeName) {
+      id
+      name
+    }
+  }
+`
+
+export const REGISTER_USER = gql`
+  mutation RegisterUser($idToken: String!, $accessToken: String!) {
+    register(idToken: $idToken, accessToken: $accessToken) {
+      name
+      email
+      id
+      createdAt
+      subscriptions {
+        kind
+        etag
+        id
+        snippet {
+          publishedAt
+          title
+          description
+          channelId
+          resourceId {
+            kind
+            channelId
+          }
+          thumbnails {
+            default {
+              url
+            }
+            medium {
+              url
+            }
+            high {
+              url
+            }
+          }
+        }
+        contentDetails {
+          totalItemCount
+          newItemCount
+          activityType
+        }
+      }
+      themes {
+        name
+        id
+      }
+    }
+  }
+`
+
+export const RELOAD_SUBS = gql`
+  mutation ReloadSubs($id: String!, $accessToken: String!) {
+    reloadSubs(id: $id, accessToken: $accessToken) {
+      kind
+      etag
+      id
+      snippet {
+        publishedAt
+        title
+        description
+        channelId
+        resourceId {
+          kind
+          channelId
+        }
+        thumbnails {
+          default {
+            url
+          }
+          medium {
+            url
+          }
+          high {
+            url
+          }
+        }
+      }
+      contentDetails {
+        totalItemCount
+        newItemCount
+        activityType
+      }
+    }
+  }
+`
