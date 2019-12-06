ca3fc339de7ea4315d9849efe9c2b0245f142716
Kevin Ostafinski <kevinostafinski@gmail.com>
2019-12-06 01:47:40 +0100

    styled LogoutButton

--- client/src/components/LogoutButton.js
+++ client/src/components/LogoutButton.js
@@ -1,4 +1,15 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Button = styled.button`
+  padding: 8px 15px;
+  border-radius: 3px;
+  border: 1px solid rgba(255,255,255,0.6);
+  background: #0c2461;
+  color: #f1f1f1;
+  font-weight: 600;
+  cursor: pointer;
+`
 
 const LogoutButton = ({ onLogoutSuccess }) => {
   const signOut = () => {
@@ -11,9 +22,9 @@ const LogoutButton = ({ onLogoutSuccess }) => {
   }
 
   return (
-    <button onClick={signOut}>
-      Logout
-    </button>
+    <Button onClick={signOut}>
+      <span>Logout</span>
+    </Button>
   )
 }
 
