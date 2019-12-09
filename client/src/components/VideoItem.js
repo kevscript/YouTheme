89ee884d51fea00a7d57e49877d2fcbc3c111966
Kevin Ostafinski <kevinostafinski@gmail.com>
2019-12-09 14:36:09 +0100

    changed videos loading flow - now shows videos on every loop instead of waiting for all videos of all channels

--- client/src/components/VideoItem.js
+++ client/src/components/VideoItem.js
@@ -5,7 +5,7 @@ const ItemContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  justify-content: top;
 `
 
 const ThumbContainer = styled.a`
